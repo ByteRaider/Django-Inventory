2062fbbb2ac004c4c2459dfcb8972e7feb33a2e1
jrms <email@fake.com>
2024-02-25 15:39:17 -0800

    Create product views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Product
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'inventory/product_list.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    success_url = reverse_lazy('product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = '__all__'
+    success_url = reverse_lazy('product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    context_object_name = 'product'
+    success_url = reverse_lazy('product_list')
+    template_name = 'inventory/product_confirm_delete.html'
