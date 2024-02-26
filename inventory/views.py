9e55234cc3c085d7268e01523642d8c93f97018a
jrms <email@fake.com>
2024-02-25 16:17:18 -0800

    Create inventory urls file

--- inventory/views.py
+++ inventory/views.py
@@ -1,15 +1,25 @@
+from django.db.models import Q
+from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+import csv
+from .models import Product, Category, Transaction
 
 # Create your views here.
 
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Product
 
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'inventory/product_list.html'
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Product.objects.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return super().get_queryset()
 
 class ProductCreateView(CreateView):
     model = Product
@@ -26,3 +36,15 @@ class ProductDeleteView(DeleteView):
     context_object_name = 'product'
     success_url = reverse_lazy('product_list')
     template_name = 'inventory/product_confirm_delete.html'
+
+def export_products_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="products.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Name', 'Category', 'Price', 'Stock'])
+
+    for product in Product.objects.all():
+        writer.writerow([product.name, product.category.name, product.price, product.stock])
+
+    return response
--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from inventory import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Include app's urls
+    path('', include('inventory.urls')),
 ]
