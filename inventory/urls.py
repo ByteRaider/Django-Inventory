d6f880fe0bd4cf9012b63bf19f3d87b197c46fe2
jrms <email@fake.com>
2024-02-25 18:02:05 -0800

    Create template files

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
+
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/add/', ProductCreateView.as_view(), name='product_add'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     # Include app's urls
     path('', include('inventory.urls')),
 ]
