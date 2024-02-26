6241e2de1d3edcd469f241390cfdfbefebe5f34d
jrms <email@fake.com>
2024-02-25 16:29:56 -0800

    Add to admin interface the models Category, Product, Transaction

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Transaction
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock')
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'transaction_type', 'created_at')
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
