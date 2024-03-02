27cfc11841ffd0eea43b0ae059ebb9809a8a1256
jrms <email@fake.com>
2024-03-01 19:44:37 -0800

    Style elements and add transitions

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -129,8 +131,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-LOGIN_REDIRECT_URL = "index"
-LOGOUT_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "product_list"
+LOGOUT_REDIRECT_URL = "product_list"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
